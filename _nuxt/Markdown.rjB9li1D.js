0f52bf9e9993bbd9cf4a6b6f6e1b9a5e0cd619cd
iepn <iepn@users.noreply.github.com>
2024-02-01 16:49:17 +0000

    deploy: 07c92767c92a473cb484ce3237114c99db1b8d49

---
+++ _nuxt/Markdown.rjB9li1D.js
@@ -0,0 +1,1 @@
+import r from"./ContentSlot._Kra5_Rr.js";import{f as o,A as f,k as u,O as c}from"./entry.jJ-kczNs.js";const i=o({name:"Markdown",extends:r,setup(t){const{parent:e}=c(),{between:n,default:a}=f(),s=u(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:s,between:n,parent:e}}});export{i as default};
